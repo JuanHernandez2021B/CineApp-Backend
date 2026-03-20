b894c16b0973d7f6d70f3277c5d6061b79388602
JuanHernandez2021B <kabezoto159@gmail.com>
2026-03-20 02:11:37 -0400

    fix cors

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Rutas
 app.use('/api/auth', authRouter);
